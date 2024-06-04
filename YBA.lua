719a86405f1ddba950d593724bdef296012e3da1
justWestl3y <156937355+justWestl3y@users.noreply.github.com>
2024-06-04 23:09:30 +0700

    Update YBA.lua

--- YBA.lua
+++ YBA.lua
@@ -1,17 +1,4 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-local colors = {
-	-- Цвет фона у Секций
-    SchemeColor = Color3.fromRGB(150, 72, 148),
-	-- Цвет фона в правой части UI
-	Background = Color3.fromRGB(15,15,15),
-	-- Цвет фона в левой части UI
-    Header = Color3.fromRGB(15,15,15),
-	-- Цвет текста
-    TextColor = Color3.fromRGB(255,255,255),
-	-- Цвет фона у кнопок
-    ElementColor = Color3.fromRGB(20, 20, 20)
-}
-
 local Window = Library.CreateLib("Name", "RJTheme3")
 local Tab = Window:NewTab("TabName")
 Section:NewLabel("LabelText")
