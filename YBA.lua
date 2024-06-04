d0e1d5dce4e62fa984c4437fcdc417ab48b0c7ac
justWestl3y <156937355+justWestl3y@users.noreply.github.com>
2024-06-04 23:30:43 +0700

    Update YBA.lua

--- YBA.lua
+++ YBA.lua
@@ -1,3 +1,4 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 local Window = Library.CreateLib("Name", "RJTheme3")
 local Tab = Window:NewTab("TabName")
 local Section = Tab:NewSection("Section Name")
