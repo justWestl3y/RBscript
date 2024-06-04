7ac269159969164d6c93bb61c672d43627c49e69
justWestl3y <156937355+justWestl3y@users.noreply.github.com>
2024-06-04 23:27:02 +0700

    Update YBA.lua

--- YBA.lua
+++ YBA.lua
@@ -1,3 +1,12 @@
+local Window = Library.CreateLib("Name", "RJTheme3")
+local Tab = Window:NewTab("TabName")
+local Section = Tab:NewSection("Section Name")
 local player = game.Players.LocalPlayer
 
-player.Character.Humanoid.WalkSpeed = 200
+Section:NewToggle("ToggleText", "ToggleInfo", function(state)
+    if state then
+        player.Character.Humanoid.WalkSpeed = 200
+    else
+        player.Character.Humanoid.WalkSpeed = 50
+    end
+end)
