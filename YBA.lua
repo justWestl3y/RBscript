d460edcafec2d4bba1882ac9c6b5c6c40d8b6e4c
justWestl3y <156937355+justWestl3y@users.noreply.github.com>
2024-06-04 23:05:41 +0700

    Create YBA.lua

---
+++ YBA.lua
@@ -0,0 +1,15 @@
+local colors = {
+	-- Цвет фона у Секций
+    SchemeColor = Color3.fromRGB(150, 72, 148),
+	-- Цвет фона в правой части UI
+	Background = Color3.fromRGB(15,15,15),
+	-- Цвет фона в левой части UI
+    Header = Color3.fromRGB(15,15,15),
+	-- Цвет текста
+    TextColor = Color3.fromRGB(255,255,255),
+	-- Цвет фона у кнопок
+    ElementColor = Color3.fromRGB(20, 20, 20)
+}
+
+local Window = Library.CreateLib("Name", "RJTheme3")
+local Tab = Window:NewTab("TabName")
