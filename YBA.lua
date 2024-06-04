83418d27b75ae9f3e3a800231c3b8c9134d246fa
justWestl3y <156937355+justWestl3y@users.noreply.github.com>
2024-06-04 23:17:34 +0700

    Update YBA.lua

--- YBA.lua
+++ YBA.lua
@@ -1,7 +1,3 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-local Window = Library.CreateLib("Name", "RJTheme3")
-local Tab = Window:NewTab("TabName")
-local Section = Tab:NewSection("Section Name")
-Section:NewButton("ButtonText", "ButtonInfo", function()
-    print("Clicked")
-end)
+local player = game.Players.LocalPlayer
+
+player.Character.Humanoid.WalkSpeed = 200
